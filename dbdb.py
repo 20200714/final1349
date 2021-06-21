b405687a0c09655380924af7f3feafa97b4438bb
bsh <20200714@g.kit.ac.kr>
2021-06-21 20:18:04 +0900

    Pythonanywhere 사이트 정보 수정

--- dbdb.py
+++ dbdb.py
@@ -11,9 +11,9 @@ def insert_data(email, pw):
 
 def get_emailpw(email, pw):
     print(email, pw)
-    db = pymysql.connect(host='backseunghun.mysql.pythonanywhere-services.com',
-                    user='backseunghun', password='asdf',
-                    db='backseunghun$mydb', charset='utf8')
+    db = pymysql.connect(host='20171062.mysql.pythonanywhere-services.com',
+                    user='20171062', password='1q2w3e4r!@',
+                    db='20171062$mydb', charset='utf8')
     c = db.cursor()
     setdata = (email, pw)
     c.execute("SELECT * FROM user_tb WHERE email = %s AND pw = %s", setdata)
